bf9371281b477d502c685f42037c3b75dd716bd4
nitslaszlo <nitslaszlo@gmail.com>
2022-04-03 17:21:58 +0200

    warning for unique indexes

--- db.bat
+++ db.bat
@@ -1,3 +1,5 @@
+@echo off
 chcp 65001
 "c:\Program Files\MongoDB\Server\5.0\bin\mongoimport.exe" --uri="mongodb://localhost:27017/AdatbázisNeve" --collection=TáblaNeve1 --drop --file=db_1.json --jsonArray
 "c:\Program Files\MongoDB\Server\5.0\bin\mongoimport.exe" --uri="mongodb://localhost:27017/AdatbázisNeve" --collection=TáblaNeveN --drop --file=db_n.json --jsonArray
+echo PLEASE KILL AND RESTART YOUR BACKEND SERVER DEV TASK IF RUNNING!
